e870ea8b714a87d780b658df815f5145184c0e66
cyyang <cyyang@pro.local>
2016-03-21 23:33:34 +0800

    modify to get basic result; no csv yet

--- label_stats.R
+++ label_stats.R
@@ -1,21 +1,17 @@
 #!/usr/bin/env Rscript
 args<-commandArgs(TRUE)
 wdir <- args[1]
-sub <- args[2]
-time <- args[3]
 
-if ( length(args) != 3 ) {
-    print("label_stats.R working_dir subject_id time_point")
-    exit()
+if ( length(args) != 1 ) {
+    stop("label_stats.R working_dir")
 }
 
-test.location = paste0(wdir, sub, "/", time, "/")
+test.location = paste0(wdir)
 
 if ( exists("test.location") ) {
     print(paste("Testing directory:", test.location))
 }  else {
-    print(paste0("Testing directory (", test.location, ") does not exist!"))
-    exit()
+    stop(paste0("Testing directory (", test.location, ") does not exist!"))
 }
 
 suppressMessages(library(ANTsR))
@@ -24,13 +20,14 @@ if ( exists("test.location") ) {
 
   mask.glob = glob2rx("*BrainExtractionMask.nii.gz")
   mask.test = list.files(path=test.location, recursive=T, full.names=T, pattern=mask.glob)[1]
-  mask.test = antsImageRead(mask.test, 3)
-  bvol.test = length(which(as.array(mask.test)>0))*prod(antsGetSpacing(mask.test))
-  mask.test = as.array(mask.test)
-  bvol.sum = length(which(mask.test>0))
+  if (exists("mask.test")) {
+      mask.test = antsImageRead(mask.test, 3)
+      bvol.test = length(which(as.array(mask.test)>0))*prod(antsGetSpacing(mask.test))
+      mask.test = as.array(mask.test)
+      bvol.sum = length(which(mask.test>0))
 
-  print(paste("Brain volume (test):", bvol.test))
-`
+      print(paste("Brain volume (test):", bvol.test))
+  }
 } else {
   print( "Need to set test.location variables" )
 }
@@ -55,7 +52,6 @@ if ( exists("test.location") ) {
     mask.sum = length(which(mask.test>0))
 
     print(paste("Volume (test):", vol.test))
-    print( " " )
 
     test.values[i] = vol.test
 
@@ -96,16 +92,18 @@ if ( exists("test.location") ) {
 
   fa.glob = glob2rx("*fa_anatomical.nii.gz")
   fa.test = list.files(path=test.location, recursive=T, full.names=T, pattern=fa.glob)[1]
+  if ( exists(fa.test) ) {
 
-  fa.test = as.array(antsImageRead(fa.test, 3))
+      fa.test = as.array(antsImageRead(fa.test, 3))
 
-  mask.test = as.array(seg.test)
-  mask.test[ mask.test != 3 ] = 0
-  mask.test[ mask.test == 3] = 1
+      mask.test = as.array(seg.test)
+      mask.test[ mask.test != 3 ] = 0
+      mask.test[ mask.test == 3] = 1
 
-  mean.test = mean( fa.test[mask.test>0] )
+      mean.test = mean( fa.test[mask.test>0] )
 
-  print(paste("Mean fractional anisotropy (test):", mean.test))
+      print(paste("Mean fractional anisotropy (test):", mean.test))
+  }
 
 
   } else {
@@ -116,37 +114,37 @@ if ( exists("test.location") ) {
 
   cbf.glob = glob2rx("*meancbf_anatomical.nii.gz")
   cbf.test = list.files(path=test.location, recursive=T, full.names=T, pattern=cbf.glob)[1]
+  if ( exists(cbf.test) ) {
+      cbf.test = as.array(antsImageRead(cbf.test, 3))
 
-  cbf.test = as.array(antsImageRead(cbf.test, 3))
+      mask.test = as.array(seg.test)
+      mask.test[ mask.test != 2 ] = 0
+      mask.test[ mask.test == 2] = 1
 
-  mask.test = as.array(seg.test)
-  mask.test[ mask.test != 2 ] = 0
-  mask.test[ mask.test == 2] = 1
+      mean.test = mean( cbf.test[mask.test>0] )
 
-  mean.test = mean( cbf.test[mask.test>0] )
+      print(paste("Mean cortical CBF (test):", mean.test))
 
-  print(paste("Mean cortical CBF (test):", mean.test))
+      mask.test = as.array(seg.test)
+      mask.test[ mask.test != 4 ] = 0
+      mask.test[ mask.test == 4] = 1
 
-  mask.test = as.array(seg.test)
-  mask.test[ mask.test != 4 ] = 0
-  mask.test[ mask.test == 4] = 1
+      mean.test = mean( cbf.test[mask.test>0] )
 
-  mean.test = mean( cbf.test[mask.test>0] )
+      print(paste("Mean deep CBF (test):", mean.test))
 
-  print(paste("Mean deep CBF (test):", mean.test))
+      logdata = data.frame(logdata, DeepGrey.MeanCBF=mean.test )
 
-  logdata = data.frame(logdata, DeepGrey.MeanCBF=mean.test )
+      motion.glob = glob2rx( "*PCASL_MOCOStatsFramewise.csv")
+      motion.test = list.files(path=test.location, recursive=T, full.names=T, pattern=motion.glob)[1]
 
-  motion.glob = glob2rx( "*PCASL_MOCOStatsFramewise.csv")
-  motion.test = list.files(path=test.location, recursive=T, full.names=T, pattern=motion.glob)[1]
+      motion.test = read.csv(motion.test)
 
-  motion.test = read.csv(motion.test)
+      n = dim(motion.test)[1]-1
+      mean.test = motion.test$Mean[1:n]
 
-  n = dim(motion.test)[1]-1
-  mean.test = motion.test$Mean[1:n]
-
-  print(paste("Displacement (test):", mean.test))
-
+      print(paste("Displacement (test):", mean.test))
+  }
   } else {
   print( "Need to set test.location variables" )
   }
@@ -155,37 +153,38 @@ if ( exists("test.location") ) {
 
   bold.glob = glob2rx("*BOLD_anatomical.nii.gz")
   bold.test = list.files(path=test.location, recursive=T, full.names=T, pattern=bold.glob)[1]
+  if ( exists(bold.test) ) {
 
-  bold.test = as.array(antsImageRead(bold.test, 3))
+      bold.test = as.array(antsImageRead(bold.test, 3))
 
-  mask.test = as.array(seg.test)
-  mask.test[ mask.test != 2 ] = 0
-  mask.test[ mask.test == 2] = 1
+      mask.test = as.array(seg.test)
+      mask.test[ mask.test != 2 ] = 0
+      mask.test[ mask.test == 2] = 1
 
-  mean.test = mean( bold.test[mask.test>0] )
+      mean.test = mean( bold.test[mask.test>0] )
 
-  print(paste("Mean cortical BOLD (test):", mean.test))
+      print(paste("Mean cortical BOLD (test):", mean.test))
 
-  mask.test = as.array(seg.test)
-  mask.test[ mask.test != 4 ] = 0
-  mask.test[ mask.test == 4] = 1
+      mask.test = as.array(seg.test)
+      mask.test[ mask.test != 4 ] = 0
+      mask.test[ mask.test == 4] = 1
 
-  mean.test = mean( bold.test[mask.test>0] )
+      mean.test = mean( bold.test[mask.test>0] )
 
-  print(paste("Mean deep BOLD (test):", mean.test))
+      print(paste("Mean deep BOLD (test):", mean.test))
 
-  motion.glob = glob2rx( "*BOLD_MOCOStatsFramewise.csv")
-  motion.test = list.files(path=test.location, recursive=T, full.names=T, pattern=motion.glob)[1]
+      motion.glob = glob2rx( "*BOLD_MOCOStatsFramewise.csv")
+      motion.test = list.files(path=test.location, recursive=T, full.names=T, pattern=motion.glob)[1]
 
-  motion.test = read.csv(motion.test)
+      motion.test = read.csv(motion.test)
 
-  n = dim(motion.test)[1]-1
-  mean.test = motion.test$Mean[1:n]
-
-
-  print(paste("Displacement (test):", mean.test))
+      n = dim(motion.test)[1]-1
+      mean.test = motion.test$Mean[1:n]
 
 
+      print(paste("Displacement (test):", mean.test))
+
+  }
   } else {
   print( "Need to set test.location variables" )
   }
