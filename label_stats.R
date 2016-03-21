22bdf23205c7c0ed57afc1a960996db7601eaa25
cyyang <cyyang@pro.local>
2016-03-22 07:53:31 +0800

    parse dkt num

--- label_stats.R
+++ label_stats.R
@@ -31,47 +31,52 @@ if (exists("mask.test")) {
 seg.names = c("CSF", "Cortex", "White matter", "Deep Gray", "BrainStem", "Cerebellum")
   #seg.glob = glob2rx("*BrainSegmentation.nii.gz")
   #seg.test = list.files(path=test.location, recursive=T, full.names=T, pattern=seg.glob)[1]
-seg.test = file.path(test.location, paste(subj, time, "BrainSegmentation.nii.gz", sep="_"))
-seg.test = antsImageRead(seg.test, 3)
-test.values = rep(0,6)
+seg.file = file.path(test.location, paste(subj, time, "BrainSegmentation.nii.gz", sep="_"))
+if (exists("seg.file")) {
+    seg.img = antsImageRead(seg.file, 3)
+    seg.array = as.array(seg.img)
+    test.values = rep(0,6)
 
-for ( i in c(1:6) ) {
-    vol.test = length(which(as.array(seg.test)==i))*prod(antsGetSpacing(seg.test))
+    for ( i in c(1:6) ) {
+	vol.test = length(which(seg.array==i))*prod(antsGetSpacing(seg.img))
+	print(paste(i, seg.names[i], "volume:", vol.test))
+	test.values[i] = vol.test
+    }
 
-    mask.test = as.array(seg.test)
-    mask.test[ mask.test != i ] = 0
-    mask.test[ mask.test == i] = 1
-
-    mask.sum = length(which(mask.test>0))
-
-    print(paste(i, seg.names[i], "volume:", vol.test))
-
-    test.values[i] = vol.test
-
+    CSF.Volume=test.values[1]
+    Cortex.Volume=test.values[2]
+    WhiteMatter.Volume=test.values[3]
+    DeepGray.Volume=test.values[4]
+    BrainStem.Volume=test.values[5]
+    Cerebellum.Volume=test.values[6]
+} else {
+    cat(paste0(seg.file, " not exists!\n"))
 }
-
-CSF.Volume=test.values[1]
-Cortex.Volume=test.values[2]
-WhiteMatter.Volume=test.values[3]
-DeepGray.Volume=test.values[4]
-BrainStem.Volume=test.values[5]
-Cerebellum.Volume=test.values[6]
 
 # Cortical thickness
 
   #thick.glob = glob2rx("*CorticalThickness.nii.gz")
   #thick.test = list.files(path=test.location, recursive=T, full.names=T, pattern=thick.glob)[1]
-  thick.test = file.path(test.location, paste(subj, time, "CorticalThickness.nii.gz", sep="_"))
+  thick.file = file.path(test.location, paste(subj, time, "CorticalThickness.nii.gz", sep="_"))
+  if(exists("thick.file"))
+  {
+      thick.array = as.array(antsImageRead(thick.file, 3))
+      mean.ct = mean( thick.array[seg.array == 2] )
+      print(paste("Mean cortical thickness:", mean.ct))
+      
+    dkt.file = file.path(test.location, paste(subj, time, "DKT.nii.gz", sep="_"))
+    if (exists("dkt.file")) {
+      data(DesikanKillianyTourville)
+      dkt.array = as.array(antsImageRead(dkt.file, 3))
+      dkt.values = unique(as.vector(dkt.array))
+      for (i in sort(dkt.values))
+	{
+	  mean.ct = mean( thick.array[dkt.array == i] )
+	  print(paste0("Mean cortical thickness (", i, "): ", mean.ct))
+	}
+    }
+  } 
 
-  thick.test = as.array(antsImageRead(thick.test, 3))
-
-  mask.test = as.array(seg.test)
-  mask.test[ mask.test != 2 ] = 0
-  mask.test[ mask.test == 2] = 1
-
-  mean.ct = mean( thick.test[mask.test>0] )
-
-  print(paste("Mean cortical thickness:", mean.ct))
 
 
 # FA
