e9b9f95c2818ba9ea15b9ff087550e1a3327b7ab
gnayyc <cyyang@ntu.edu.tw>
2018-02-12 13:20:57 +0800

    revision; note: need pandoc.exe in the same directory

---
+++ derma/0schedule.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+
+local({r <- getOption("repos"); 
+    r["CRAN"] <- "https://cloud.r-project.org/";
+    options(repos = r)})
+list.of.packages <- c("tidyverse", "lubridate", "readxl", "writexl", "knitr", "devtools")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+if(!require(rmarkdown)) 
+{
+    devtools::install_github(c('rstudio/rmarkdown'))
+}
+
+library(lubridate)
+ofile = paste0("schedule-", today(),".html")
+rmarkdown::render('0schedule.Rmd', 
+    output_format = "html_document",
+    output_file = ofile)
+shell(ofile)
--- derma/0schedule.bat
+++ derma/0schedule.bat
@@ -1,3 +1,1 @@
-R-3.4.3\bin\Rscript.exe schedule.R
-start today.xlsx
-start week.xlsx
+R-3.4.3\bin\Rscript.exe 0schedule.R
