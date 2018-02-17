b56c1f9f43e8f76eb4b38a09f3f0e3bb4276473c
gnayyc <cyyang@ntu.edu.tw>
2018-02-18 00:08:53 +0800

    use googlesheets produce a to_register_list and an all_lesion_list

--- derma/0schedule.R
+++ derma/0schedule.R
@@ -3,7 +3,7 @@
 local({r <- getOption("repos"); 
     r["CRAN"] <- "https://cloud.r-project.org/";
     options(repos = r)})
-list.of.packages <- c("tidyverse", "lubridate", "readxl", "writexl", "knitr", "devtools", "stringr")
+list.of.packages <- c("tidyverse", "lubridate", "knitr", "devtools", "stringr", "googlesheets", "kableExtra")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 if(!require(rmarkdown)) 
@@ -11,21 +11,26 @@ if(!require(rmarkdown))
     devtools::install_github(c('rstudio/rmarkdown'))
 }
 
-library(lubridate)
-
 dir.create("schedule")
-ofile = paste0("schedule/schedule-", today(),".html")
+ofile = paste0("schedule/schedule-", format(Sys.time(), "%Y-%m-%d"),".html")
+afile = paste0("schedule/schedule_all-", format(Sys.time(), "%Y-%m-%d"),".html")
 
 
 rmarkdown::render('0schedule.Rmd', 
     output_format = "html_document",
     output_file = ofile)
 
+rmarkdown::render('0schedule_all.Rmd', 
+    output_format = "html_document",
+    output_file = afile)
+
 if (.Platform$OS.type == "unix")
 {
     system(paste0("open ", ofile))
+    system(paste0("open ", afile))
 } else
 {
     shell(str_replace(ofile, "/", "\\\\"))
+    shell(str_replace(afile, "/", "\\\\"))
 }
 
