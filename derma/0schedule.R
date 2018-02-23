ecfa1258f61dfbed30a4f417288de7efed61ea59
gnayyc <cyyang@ntu.edu.tw>
2018-02-23 22:59:52 +0800

    deal with NA

--- derma/0schedule.R
+++ derma/0schedule.R
@@ -16,18 +16,20 @@ library(lubridate)
 library(googlesheets)
 
 dir.create("schedule")
-# https://docs.google.com/spreadsheets/d/18NQX3J2LxYY7K_FJgmk_Sf88G0-a64SxCj_c8OxQgd8
-# https://docs.google.com/spreadsheets/d/e/2PACX-1vTEAIEr8YVek24JYDSHFdueJ_cOclaSQWLA8IgpXLJT1a-RkQdifhmB1nEMAMb1LKzCbfs8aM09S4lQ/pubhtml
 
 gs_auth()
 
 derm = 
-    gs_key("18NQX3J2LxYY7K_FJgmk_Sf88G0-a64SxCj_c8OxQgd8") %>%
+    #gs_key("18NQX3J2LxYY7K_FJgmk_Sf88G0-a64SxCj_c8OxQgd8") %>%
+    gs_url("https://docs.google.com/spreadsheets/d/18NQX3J2LxYY7K_FJgmk_Sf88G0-a64SxCj_c8OxQgd8") %>%
     gs_read() %>%
     set_names(c("time","CID","name","lesion","photo_date","registry_date","interval")) %>%
+    group_by(CID) %>%
     mutate(photo_date = ymd(photo_date), # register date 
            registry_date = ymd(registry_date), # register date 
-	   lesion = as.character(lesion)) %>%
+	   lesion = as.character(lesion),
+	   is_last_registry = ifelse(registry_date %in% ymd(max(registry_date, na.rm=T)), T, F),
+	   interval = ifelse(is.na(interval), 6, interval)) %>%
     arrange(CID, lesion, registry_date) %>%
     group_by(CID, lesion) %>%
     filter(row_number() == which.max(photo_date)) %>%
@@ -44,32 +46,27 @@ d =
   derm %>% 
   group_by(CID) %>% 
   summarise(
-    last_registry_date = max(registry_date, na.rm=T),
-    last_lesion = lesion[which(registry_date == max(registry_date, na.rm=T))]) %>% 
+    last_registry_date = ymd(max(registry_date, na.rm=T))
+    #,last_lesion = ifelse(any(is_last_registry), lesion[which(is_last_registry)], NA),
+    #last_lesion = lesion[which.max(registry_date)]) %>% 
+    #last_lesion = ifelse(any(lesion[which.max(registry_date)]), lesion[which.max(registry_date)], NA)) %>% 
+  ) %>%
   right_join(derm) %>%
-  mutate(
-    i        = interval(ymd(registry_date),      ymd(today()-1            )) %/% months(1),
-    i7       = interval(ymd(registry_date),      ymd(today()-1 + weeks(1) )) %/% months(1),
-    i30      = interval(ymd(registry_date),      ymd(today()-1 + months(1))) %/% months(1),
-    i_last   = interval(ymd(last_registry_date), ymd(today()-1            )) %/% months(1),
-    i7_last  = interval(ymd(last_registry_date), ymd(today()-1 +  weeks(1))) %/% months(1),
-    i30_last = interval(ymd(last_registry_date), ymd(today()-1 + months(1))) %/% months(1)
-  ) %>% 
   mutate(
          next_photo_date = photo_date + months(interval),
          next_registry_date = case_when(
+	    is.na(last_registry_date) ~ photo_date,
 	    is.na(registry_date) ~ last_registry_date + months(1),
 	    registry_date + months(6) > last_registry_date + months(1) ~ registry_date + months(6),
 	    T ~ last_registry_date + months(1)
         )
   ) %>%
   mutate(
-    #l0 = ifelse(show0, paste0(lesion, " (",registry_date,")"), NA),
-    #l7 = ifelse(show7, paste0(lesion, " (",registry_date,")"), NA),
-    #l30 = ifelse(show30, paste0(lesion, " (",registry_date,")"), NA),
-    l0 = ifelse(ymd(today()) > next_registry_date, paste0(lesion, " (",registry_date,")"), NA),
-    l7 = ifelse(ymd(today()+weeks(1)) > next_registry_date, paste0(lesion, " (",registry_date,")"), NA),
-    l30 = ifelse(ymd(today()+months(1)) > next_registry_date, paste0(lesion, " (",registry_date,")"), NA)
+    l0 = ifelse(ymd(today()) > next_registry_date, paste0(lesion, " (",registry_date,")"), NA)
+    ,l7 = ifelse(ymd(today()+weeks(1)) > next_registry_date, paste0(lesion, " (",registry_date,")"), NA)
+    ,l30 = ifelse(ymd(today()+months(1)) > next_registry_date, paste0(lesion, " (",registry_date,")"), NA)
+    ,l180 = ifelse(ymd(today()+months(1)) <= next_registry_date, paste0(lesion, " (",registry_date,")"), NA)
+
   ) 
 
 dfile = "schedule/schedule.Rdata"
