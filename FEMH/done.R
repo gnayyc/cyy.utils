1fa26fcc51402c373fc7372cc705570e97695590
Chung-YiYang <cyyang@cyyang.Home>
2018-07-22 22:42:31 +0800

    add xmldir check

--- FEMH/done.R
+++ FEMH/done.R
@@ -1,41 +1,56 @@
 #!/usr/bin/env Rscript
 
-# move labeled files into 0done
+# move labeled files into 0label
 # move unlabeled files into 0undtermined
+# move xml files into 0xml or xmldir
 
 args <- commandArgs(TRUE)
 
 # test if there is at least one argument: if not, return an error
 if (length(args)<1) {
-  stop("done.R dir\n", call.=F)
+  stop("done.R imgdir [xmldir]\n", call.=F)
 } 
 
-dir = args[1]
-if (!dir.exists(dir)) {
-  stop(paste0(dir, " not exists\n"))
+imgdir = args[1]
+if (!dir.exists(imgdir)) {
+  stop(paste0(imgdir, " not exists\n"))
 } 
+if (length(args) >= 2)
+{
+    xmldir = args[2]
+    if (!dir.exists(xmldir)) {
+      stop(paste0(xmldir, " not exists\n"))
+    } 
+}
 
-dir.done = file.path(dir, "0done")
-dir.xml = file.path(dir, "0xml")
-dir.und = file.path(dir, "0undetermined")
-dir.create(dir.done)
-dir.create(dir.und)
 
-png = list.files(dir, "*.png")
-xml = list.files(dir, "*.xml")
-file.done = tools::file_path_sans_ext(xml)
-file.all = tools::file_path_sans_ext(png)
-done.i = which(file.all %in% file.done)
+labeldir = file.path(imgdir, "0label")
+unddir = file.path(imgdir, "0undetermined")
+if (length(args) < 2)
+{
+    xmldir = file.path(imgdir, "0xml")
+}
 
-for (i in 1:max(done.i)) {
-    if (i %in% done.i) {
-	file.rename(file.path(dir, paste0(file.all[i],".png")), 
-	            file.path(dir.done, paste0(file.all[i],".png")))
-	file.rename(file.path(dir, paste0(file.all[i],".xml")), 
-	            file.path(dir.xml, paste0(file.all[i],".xml")))
+dir.create(labeldir)
+dir.create(unddir)
+
+png = list.files(imgdir, "*.png")
+xml = c(list.files(imgdir, "*.xml"), list.files(imgdir, "*.xml"))
+file.label = tools::file_path_sans_ext(xml)
+file.img = tools::file_path_sans_ext(png)
+label.i = which(file.img %in% file.label)
+
+for (i in 1:max(label.i)) {
+    if (i %in% label.i) {
+	file.rename(file.path(imgdir, paste0(file.img[i],".png")), 
+	            file.path(labeldir, paste0(file.img[i],".png")))
+	if(file.exists(file.path(imgdir, paste0(file.img[i],".xml")))) {
+	    file.rename(file.path(imgdir, paste0(file.img[i],".xml")), 
+			file.path(xmldir, paste0(file.img[i],".xml")))
+	}
     } else {
-	file.rename(file.path(dir, paste0(file.all[i],".png")), 
-	            file.path(dir.und, paste0(file.all[i],".png")))
+	file.rename(file.path(imgdir, paste0(file.img[i],".png")), 
+	            file.path(unddir, paste0(file.img[i],".png")))
     }
 }
 
