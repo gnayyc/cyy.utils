460090981c06508ee670caf8981e21b1bc4fe97f
Chung-YiYang <cyyang@cyyang.Home>
2018-07-28 23:25:35 +0800

    convert pascal voc xml files into GUANTA csv format

---
+++ FEMH/bbox_xml2csv.R
@@ -0,0 +1,26 @@
+#!/usr/bin/env Rscript
+
+library(magrittr)
+library(xml2)
+
+f = list.files(".", "*.xml")
+ACCNO = character(0)
+bbox = character(0)
+for (i in seq_along(f)) {
+    x = f[i] %>% read_xml()
+    accno = x %>% xml_find_all(".//filename") %>% xml_text %>% sub(".png", "", .)
+    node_box = x %>% xml_find_all(".//bndbox")
+    bndbox = ""
+    for (b in node_box) {
+	xmin = b %>% xml_find_all(".//xmin") %>% xml_text
+	ymin = b %>% xml_find_all(".//ymin") %>% xml_text
+	xmax = b %>% xml_find_all(".//xmax") %>% xml_text
+	ymax = b %>% xml_find_all(".//ymax") %>% xml_text
+	bndbox = paste(paste0("(", paste0(c(xmin,ymin,xmax,ymax),collapse=". ") ,")"), bndbox)
+    }
+    ACCNO[i] = accno
+    bbox[i] = bndbox
+    
+}
+
+data.frame(ACCNO, bbox) %>% readr::write_csv("bbox.csv")
