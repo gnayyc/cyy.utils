07c4b4d285fb69c51f6b78c2434b84762ad34e70
gnayyc <cyyang@ntu.edu.tw>
2020-05-13 22:45:43 +0800

    add action and ext ability

--- FEMH/getlist.jl
+++ FEMH/getlist.jl
@@ -1,28 +1,30 @@
 #!/usr/bin/env julia
 
-if length(ARGS) < 3
-    error(PROGRAM_FILE, " csv_with_ACCNO source_dir output_dir [iid_column] [key_column] [key2_column]")
+if length(ARGS) < 5
+    println(PROGRAM_FILE, " csv_with_ACCNO ln|cp|mv png|dcm source_dir output_dir [iid_column] [key_column] [key2_column]")
+#			    1		    2	    3		    4		5	    6		7	    8
     exit()
 end
 
-ext = ".dcm"
 csv = ARGS[1]
-dir1 = ARGS[2]
-dir2 = ARGS[3]
+action = ARGS[2]
+ext = ARGS[3]
+dir1 = ARGS[4]
+dir2 = ARGS[5]
 
 println("csv file: ", csv)
 using CSV
 x = CSV.read(csv)
 
-if length(ARGS) >= 4
-    id = ARGS[4]
-    if length(ARGS) >= 5
-	key = ARGS[5]
-	if length(ARGS) == 5
+if length(ARGS) >= 6
+    id = ARGS[6]
+    if length(ARGS) >= 7
+	key = ARGS[7]
+	if length(ARGS) == 7
 	    csv2 = splitext(csv)[1] * "-" * key * ".csv"
 	    x[:, "to"] = dir2 .* "/" .* key .* "-" .* x[:, id] .* ext
 	else
-	    key2 = ARGS[6]
+	    key2 = ARGS[8]
 	    csv2 = splitext(csv)[1] * "-" * key * "-" * key2 * ".csv"
 	    x[:, "to"] = dir2 .* "/" .* key .* "-" .* key2 .* "-" .* x[:, id] .* ext
 	end
@@ -54,7 +56,14 @@ println("Copy from ", dir1, " to ", dir2)
 for i = 1:size(x, 1)
     from = x[i, "from"]
     to = x[i, "to"]
-    cmd = `ln $from $to`
+    if cmd == "mv"
+	cmd = Sys.iswindows() ? `move $from $to` : `mv $from $to`
+    elseif cmd == "ln"
+	cmd = Sys.iswindows() ? `copy $from $to` : `ln $from $to`
+    else # cp
+	cmd = Sys.iswindows() ? `copy $from $to` : `cp $from $to`
+
+    
     try
 	run(cmd)
     catch e
