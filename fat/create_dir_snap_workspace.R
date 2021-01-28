d230b32462b05d7dd2999288c85b340c33a3ea11
Chung-YiYang <cyyang@g.ntu.edu.tw>
2021-01-28 19:48:15 +0800

    consider 3d dimensions

--- fat/create_dir_snap_workspace.R
+++ fat/create_dir_snap_workspace.R
@@ -5,7 +5,7 @@ file.arg.name <- "--file="
 script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
 Sys.setenv(script_dir = dirname(script.name))
 
-create_snap_workspace = function(id = NA, template = "template.itksnap", directory = ".", file = paste0(id, ".itksnap")) {
+create_snap_workspace = function(id = NA, template = "template.itksnap", directory = ".", file = paste0(id, ".itksnap"), dim_x = 512, dim_y = 512, dim_z = 1) {
     library(magrittr)
     library(xml2)
     library(stringr)
@@ -20,19 +20,28 @@ create_snap_workspace = function(id = NA, template = "template.itksnap", directo
     .entries = .x %>% xml_find_all("//registry//folder/folder/entry")
     for (.e in .entries) {
 	if(xml_attr(.e, "key") == "AbsolutePath") xml_set_attr(.e, "value", str_replace(xml_attr(.e, "value"), "id", id))
+	if(xml_attr(.e, "key") == "Dimensions") xml_set_attr(.e, "value", paste(dim_x, dim_y, dim_z))
+	if(xml_attr(.e, "key") == "ResampleDimensions") xml_set_attr(.e, "value", paste(dim_x, dim_y, dim_z))
     }
     write_xml(.x, file.path(directory, file))
     
 }
 
 library(stringr)
+library(oro.nifti)
 
 s.dirs = list.dirs(".", full.names = T, recursive = F)
 for (s.dir in s.dirs) {
     .ii = dir(s.dir, "*.nii.gz")
-    for (.i in .ii) {
-	.id = str_remove(.i, ".nii.gz")
-	create_snap_workspace(.id, directory = s.dir)
+    if (length(.ii) > 0) {
+	.i = .ii[1]
+	.nii = oro.nifti::readNIfTI(file.path(s.dir, .i))
+	if (oro.nifti::is.nifti(.nii)) {
+	    .img = oro.nifti::img_data(.nii)
+	    dim(.img)
+	    .id = str_remove(.i, ".nii.gz")
+	    create_snap_workspace(.id, directory = s.dir, dim_x = dim(.img)[1], dim_y = dim(.img)[2], dim_z = dim(.img)[3])
+	}
     }
 }
 
