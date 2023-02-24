cd7602695f30cf7c2909dde2f8dc746a50ea68b0
Yang Chung-Yi <cyyang@sun.local>
2023-02-24 12:32:51 +0800

    deal with rescale and smooth

--- dicom/dcmdir_window.jl
+++ dicom/dcmdir_window.jl
@@ -1,6 +1,7 @@
 #!/usr/bin/env julia
 
 using DICOM
+using ImageFiltering
 
 dcmDir = "."
 dcmDir2 = joinpath(dcmDir, "work")
@@ -9,6 +10,9 @@ mkpath(dcmDir2)
 for (i, dcmFile) in enumerate(DICOM.find_dicom_files(dcmDir))
     #println(dcmFile)
     img = DICOM.dcm_parse(joinpath(dcmDir, dcmFile))
-    img.PixelData[img.PixelData .< -300] .= -2048
+    p = img.PixelData .* img.RescaleSlope .+ img.RescaleIntercept
+    p[p .< -700] .= -1024
+    p = Int.(round.(imfilter(p, Kernel.gaussian(3))))
+    img.PixelData .= (p .- img.RescaleIntercept) ./ img.RescaleSlope
     DICOM.dcm_write(joinpath(dcmDir2, dcmFile), img)
 end
