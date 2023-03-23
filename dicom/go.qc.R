8982b5ef650d4086e3737f37aaf637a0353d4cf3
Yang Chung-Yi <cyyang@sun.local>
2023-03-23 22:00:48 +0800

    generate go_CT_list.csv for check

--- dicom/go.qc.R
+++ dicom/go.qc.R
@@ -2,6 +2,12 @@
 
 system('dcmdir2csv.py')
 x = data.table::fread("info.csv")
+y = unique(x[!is.na(StudyDate),.(PatientID,StudyDate,StudyTime,InstitutionName,ManufacturerModelName,Manufacturer,SeriesDescription,SliceThickness)], by = c("PatientID", "StudyDate"))[order(PatientID, StudyDate)]
+#y[,n_Exam := seq_len(.N), by=.(PatientID)][,N_Exam:= .N, by=.(PatientID)]
+y[,n_Exam := data.table::rowid(PatientID)][,N_Exam:= .N, by=.(PatientID)]
+z = y[,.(PatientID,StudyDate,StudyTime,n_Exam,N_Exam,InstitutionName,ManufacturerModelName,Manufacturer,SeriesDescription,SliceThickness)]
+
+data.table::fwrite(z, "GO_CT_list.csv")
 
 cat("==== min(SliceThickness) ====\n")
 x[!is.na(SliceThickness)][,.(SliceThickness = min(SliceThickness)), by = .(PatientID, StudyDate)][order(PatientID, StudyDate)]
